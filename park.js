e7e20206a90fe984d232d29182b60be508e5d81c
Michael Shennan <m.shennan22@gmail.com>
2017-05-09 20:08:22 +0100

    can remove dinosaurs by type

--- park.js
+++ park.js
@@ -8,6 +8,15 @@ Park.prototype = {
     this.enclosure.push(dinosaur);
   },
 
+  removeDinosaurByType: function(type) {
+    for (var dinosaur of this.enclosure) {
+      if (dinosaur.type === type) {
+        var index = this.enclosure.indexOf(type);
+        this.enclosure.splice(index, 1);
+      }
+    }
+  },
+
 }
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -8,12 +8,16 @@ describe("Park", function() {
   var dinosaur1;
   var dinosaur2;
   var dinosaur3;
+  var dinosaur4;
+  var dinosaur5;
 
   beforeEach(function() {
     park = new Park();
     dinosaur1 = new Dinosaur({type: "Tyrannosaurus", offspring: 1});
     dinosaur2 = new Dinosaur({type: "Velociraptor", offspring: 5});
     dinosaur3 = new Dinosaur({type: "Triceratops", offspring: 3});
+    dinosaur4 = new Dinosaur({type: "Triceratops", offspring: 4});
+    dinosaur5 = new Dinosaur({type: "Pterodactyl", offspring: 8});
   });
 
   it("should start empty", function() {
@@ -23,6 +27,16 @@ describe("Park", function() {
   it("can add a dinosaur to the enclosure", function() {
     park.addDinosaur(dinosaur1);
     assert.equal(1, park.enclosure.length);
+  });
+
+  it("can remove dinosaurs of a particular type", function() {
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    park.removeDinosaurByType("Triceratops");
+    assert.equal(3, park.enclosure.length);
   });
 
 });
